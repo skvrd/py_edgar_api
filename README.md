d6d395ee870fd2996a553903c40562d0acc61be5
grisha <grisha@grisha.co>
2024-04-11 10:56:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple SDK to work with SEC EDGAR.
---
+++ py_edgar_api/client.py
@@ -0,0 +1,4 @@
+class Client():
+    def __init__(self, company_name: str, email: str) -> None:
+        self._company_name = company_name
+        self._email = email
